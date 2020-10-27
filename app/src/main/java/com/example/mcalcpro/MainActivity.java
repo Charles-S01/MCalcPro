8298ffdd04b1a93e2c6f7739d4fdfd41b10409e3
Charles-S01 <charlesangalang1@gmail.com>
2020-10-27 13:09:19 -0400

    Final1

--- app/src/main/java/com/example/mcalcpro/MainActivity.java
+++ app/src/main/java/com/example/mcalcpro/MainActivity.java
@@ -2,20 +2,63 @@ package com.example.mcalcpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
 import ca.roumani.i2c.MPro;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, SensorEventListener {
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tts = new TextToSpeech(this, this);
+
+//        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        sm.registerListener(this, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    public void onInit(int initStatus)
+    {
+        this.tts.setLanguage(Locale.US);
+    }
+
+    public void onAccuracyChanged(Sensor arg0, int arg1)
+    {
+    }
+
+    public void onSensorChanged(SensorEvent event)
+    {
+        double ax = event.values[0];
+        double ay = event.values[1];
+        double az = event.values[2];
+
+        double a = Math.sqrt(ax*ax + ay*ay + az*az);
+
+        if (a > 20)
+        {
+            ((EditText) findViewById(R.id.pBox)).setText(null);
+            ((EditText) findViewById(R.id.aBox)).setText(null);
+            ((EditText) findViewById(R.id.iBox)).setText(null);
+            ((TextView) findViewById(R.id.output)).setText(null);
+        }
     }
 
     public void buttonClicked(View V)
@@ -43,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             ((TextView) findViewById(R.id.output)).setText(s);
+            tts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
         }
         catch (Exception e)
         {
